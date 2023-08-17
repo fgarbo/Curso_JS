413b2123ce3b076d8b17548d3dc1d3cce2e29faf
Flavio Garbo <fgarbo@minsait.com>
2023-08-17 18:45:44 -0300

    2#

---
+++ LivroJs/Cap_2/resp1_b.js
@@ -0,0 +1,18 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#outPromocao")
+const resp2 = document.querySelector("#outPrecoTerceiro")
+
+
+frm.addEventListener("submit", (e) =>{
+    const produto = frm.inProduto.value
+    const preco = Number(frm.inPreco.value)
+
+    const terceiro = preco / 2;
+    const total = (preco * 2) + terceiro;
+
+    const promocao = Math.floor(preco*3) - 0.50
+    resp1.innerText = `${produto} - Promoção: Leve 3 por R$: ${total. toFixed(2)}`
+    resp2.innerText = `O 3º produto custa apenas R$: ${terceiro. toFixed(2)}`
+
+    e.preventDefault()
+})
